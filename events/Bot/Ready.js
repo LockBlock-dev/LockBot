d302bbb5ca46b7f79d93cd9549cc8dc3c7b170e2
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2020-08-15 12:04:22 +0200

    Update Ready.js

--- events/Bot/Ready.js
+++ events/Bot/Ready.js
@@ -4,5 +4,5 @@ const bot = require("../../index.js")
 bot.on("ready", () => {
     console.log(chalk.red("[Bot]") + ` Activated with ${bot.users.cache.size} users, in ${bot.guilds.cache.size} servers.`)
     bot.user.setStatus('Online')
-    bot.user.setActivity(`+help | in ${bot.guilds.cache.size} servers`, { type: 'PLAYING' })
+    bot.user.setActivity(`+help | in ${bot.guilds.cache.size} servers`, { type: 'WATCHING' })
 })
