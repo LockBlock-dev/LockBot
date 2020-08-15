62395b7c3ac73fe35b3afa5b3ef49a79a1f8ea91
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2020-08-15 19:46:12 +0200

    Update Message.js

--- events/Bot/Message.js
+++ events/Bot/Message.js
@@ -54,18 +54,18 @@ bot.on("message", async message => {
   
     return message.reply(lang.messageEventMissingPermManageGuild)
 
-    else if (commandfile.help.isUserMod && !message.member.hasPermission('MANAGE_MESSAGES'))
+    if (commandfile.help.isUserMod && !message.member.hasPermission('MANAGE_MESSAGES'))
   
     return message.reply(lang.messageEventMissingPermManageMessages)
 
-      if (commandfile.help.args && !args.length) {
-        var noArgsReply = lang.messageEventCommandNeedArg + `<@${message.author.id}> !`
+     if (commandfile.help.args && !args.length) {
+       var noArgsReply = lang.messageEventCommandNeedArg + `<@${message.author.id}> !`
   
-      if (commandfile.help.usage) noArgsReply += `\n` + lang.messageEventCommandUsage + `\`${prefix}${commandfile.help.name} ${commandfile.help.usage}\``
+     if (commandfile.help.usage) noArgsReply += `\n` + lang.messageEventCommandUsage + `\`${prefix}${commandfile.help.name} ${commandfile.help.usage}\``
   
-      return message.channel.send(noArgsReply)
-        }
+     return message.channel.send(noArgsReply)
+       }
   
   
-      if(commandfile) commandfile.run(bot, message, args)
+     if(commandfile) commandfile.run(bot, message, args)
 })
