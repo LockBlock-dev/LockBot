4af63ae5d54dbd1af814aa05e3cc603e5d05cda4
LockBlock <LockBlock-dev@protonmail.com>
2020-12-28 17:21:50 +0100

    fixing ready.js

--- events/Bot/Ready.js
+++ events/Bot/Ready.js
@@ -5,8 +5,4 @@ bot.on("ready", () => {
     console.log(chalk.red("[Bot]") + ` Activated with ${bot.users.cache.size} users, in ${bot.guilds.cache.size} servers.`)
     bot.user.setStatus('Online')
     bot.user.setActivity(`+help | in ${bot.guilds.cache.size} servers`, { type: 'WATCHING' })
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 1b8ca7a4ed82a8fc07a3c74754fc6dda59b5ce2f
