faf52e80db2e7cba33cd26e2408f1cd5a8c744bb
LockBlock <LockBlock-dev@protonmail.com>
2020-12-29 12:07:28 +0100

    fix

--- commandes/Dev/Restart.js
+++ commandes/Dev/Restart.js
@@ -1,20 +1,19 @@
 const { MESSAGES } = require("../../core/constants.js")
 const Discord = require("discord.js")
 const chalk = require("chalk")
-const config = require("../../config.json")
+require('dotenv').config()
 
 module.exports.run = async (bot, message) => {
 
     if (message.author.id !== "249899689028091904") return message.reply("LockBot isn't your bot !")
 	message.delete()
-    message.channel.send("Redémarrage...")
+    message.channel.send("Restarting...")
     .then(m => m.delete(100))
-    .then(bot.destroy())
-    .then(() => bot.login(config.token))
+    .then(process.exit())
     console.log(chalk.red("[Bot] ") + "Restart command: LOADING.")
     message.channel.send("Restart successful !")
 
-console.log(chalk.red("[Bot] ") + "Restart command: SUCCESS.")
+    console.log(chalk.red("[Bot] ") + "Restart command: SUCCESS.")
 }
 
 module.exports.help = MESSAGES.COMMANDS.DEV.RESTART
