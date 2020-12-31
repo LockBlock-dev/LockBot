cf90f8dd92d46959980fd73226c996fd62d5fa00
LockBlock <LockBlock-dev@protonmail.com>
2020-12-31 22:49:32 +0100

    Added a command to add all servers the bot is in who arent in DB to prevent any errors

---
+++ commandes/Dev/AddServersInDB.js
@@ -0,0 +1,28 @@
+const { MESSAGES } = require("../../core/constants.js")
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    message.delete()
+
+    if (message.author.id !== "249899689028091904") return message.reply("LockBot isn\'t your bot !")
+
+    const guilds = bot.guilds.cache.map(guild => [guild.id, guild.name])
+    console.log(guilds)
+
+    for (const server in guilds) {
+        const test = await bot.getGuild(guilds[server][0])
+        if (typeof test === 'undefined') {
+            const newGuild = {
+                guildID: guilds[server][0],
+                guildName: guilds[server][1]
+              }
+            
+              await bot.createGuild(newGuild)
+        }
+    }
+}
+
+
+
+module.exports.help = MESSAGES.COMMANDS.DEV.ASIDB
