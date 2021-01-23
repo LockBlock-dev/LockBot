b01fbf60db4f2cbe9c7f16d9008e86331b1f64fe
LockBlock <LockBlock-dev@protonmail.com>
2021-01-23 12:49:05 +0100

    Added delete function to Leave event

--- events/Guild/GuildLeave.js
+++ events/Guild/GuildLeave.js
@@ -1,18 +1,8 @@
 const bot = require("../../index.js")
-const fs = require("fs")
-const path = require('path')
 
-bot.on('guildDelete', guild => {
+bot.on('guildDelete', async guild => {
 
-	const ID = guild.id
+	await bot.deleteGuild(guild)
 
 	bot.user.setActivity(`+help | in ${bot.guilds.cache.size} servers`, { type: 'WATCHING' })
-
-	const settingsPath = path.join(__dirname, "../../core/guildSettings.json")
-	try {
-		var GUILDS = JSON.parse(fs.readFileSync(settingsPath, "utf8"))
-		delete GUILDS[ID]
-		return fs.writeFileSync(settingsPath, JSON.stringify(GUILDS))
-    } catch (error) {
-	}
 })
