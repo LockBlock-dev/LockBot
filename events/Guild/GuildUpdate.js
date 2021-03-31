fb435a54cd7b42ee5f1b907ed5e9d31d29fb1986
LockBlock <LockBlock-dev@protonmail.com>
2021-03-31 17:49:48 +0200

    check if the name is modified, if not do nothing

--- events/Guild/GuildUpdate.js
+++ events/Guild/GuildUpdate.js
@@ -3,7 +3,9 @@ const chalk = require("chalk")
 
 bot.on("guildUpdate", async (oldGuild, newGuild) => {
 
-    await bot.updateGuild(newGuild.id, { guildName: newGuild.name})
+    if (oldGuild.name !== newGuild.name) {
+        await bot.updateGuild(newGuild.id, { guildName: newGuild.name})
 
-	console.log(`${chalk.red("[Bot]")} Guild edited : ${newGuild.id} ${oldGuild.name} => ${newGuild.name}.`)
+	    console.log(`${chalk.red("[Bot]")} Guild edited : ${newGuild.id} ${oldGuild.name} => ${newGuild.name}.`)
+    }
 })
