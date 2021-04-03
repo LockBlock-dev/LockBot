bca33e17c97f2f890d5d4610615eef33dd9b0dda
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-04-03 19:50:19 +0200

    update userInfos command with joined server date

--- commandes/Infos/UserInfos.js
+++ commandes/Infos/UserInfos.js
@@ -1,14 +1,14 @@
 const { COMMANDS } = require("../../core/constants.js")
 const Discord = require("discord.js")
 
-function checkDays(date) {
-  const now = new Date()
-  const diff = now.getTime() - date.getTime()
-  const days = Math.floor(diff / 86400000)
-  return days + (days == 1 ? " day" : " days") 
-}
-
 module.exports.run = (bot, message, args, settings, lang) => {
+
+  function checkDays(date) {
+    const now = new Date()
+    const diff = now.getTime() - date.getTime()
+    const days = Math.floor(diff / 86400000)
+    return days + (days == 1 ? lang.userInfoDay : lang.userInfoDays) 
+  }
 
   var member
      
@@ -42,17 +42,20 @@ module.exports.run = (bot, message, args, settings, lang) => {
   } else {
     boost = lang.userInfoBoostNo
   }
+
+  const guildMember = message.guild.members.cache.get(member.id)
   
   const embed = new Discord.MessageEmbed()
     .setDescription(`<@${message.author.id}>`)
     .setThumbnail(member.avatarURL({ format: 'png', dynamic: true}))
     .addField(lang.userInfoUsername,`<@${member.id}>`, true)
-    .addField(lang.userInfoNickname, `${message.guild.members.cache.get(member.id).nickname !== null ? `${message.guild.members.cache.get(member.id).nickname}` : lang.userInfoNoNickname}`, true)
+    .addField(lang.userInfoNickname, `${guildMember.nickname !== null ? `${guildMember.nickname}` : lang.userInfoNoNickname}`, true)
     .addField("Bot ?", bot, true)
     .addField("Booster", boost, true)
     .addField("ID", member.id,true)
-    .addField(lang.userInfoJoinedDate, `${member.createdAt.toUTCString().substr(0, 16)} (${checkDays(member.createdAt)})`)
-    .addField("Roles", `${message.guild.members.cache.get(member.id).roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "Pas de rôles"}`)
+    .addField(lang.userInfoJoinedDate, `${guildMember.joinedAt.toUTCString().substr(0, 16)} (${checkDays(guildMember.joinedAt)})`)
+    .addField(lang.userInfoCreatedDate, `${member.createdAt.toUTCString().substr(0, 16)} (${checkDays(member.createdAt)})`)
+    .addField("Roles", `${guildMember.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "Pas de rôles"}`)
     .setColor("#FF8A33")
     .setFooter("© LockBot")
     .setTimestamp()
