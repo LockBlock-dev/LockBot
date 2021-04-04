160187187424f5a46d5ef5e76ba11a15d54e2389
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-04-04 18:16:25 +0200

    embeded command

--- commands/LockBot/Invite.js
+++ commands/LockBot/Invite.js
@@ -2,7 +2,14 @@ const { COMMANDS } = require("../../core/constants.js")
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message) => {
-    message.channel.send("https://discord.com/api/oauth2/authorize?client_id=812368677726060586&permissions=519249&scope=bot")
+    const embed = new Discord.MessageEmbed()
+		.setDescription(`<@${message.author.id}>`)
+		.addField("URL :","https://discord.com/api/oauth2/authorize?client_id=812368677726060586&permissions=519249&scope=bot")
+		.setColor("#FF8A33")
+		.setFooter("© LockBot")
+		.setTimestamp()
+	
+	message.channel.send(embed)
 }
 
 
