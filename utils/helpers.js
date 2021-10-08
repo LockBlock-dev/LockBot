9b664f3901f601024bbf16ac18bec8f466d480dc
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-10-08 20:29:40 +0200

    humanize-duration

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,24 +1,29 @@
-const { MessageEmbed } = require('discord.js')
+const humanize = require("humanize-duration");
+const { MessageEmbed } = require("discord.js");
 
-module.exports = client => {
-    client.newError = error => {
-        const embed = new MessageEmbed()
-            .setDescription(error)
-            .setColor('#FF0000')
-    
-        return embed
-    }
+const shortHumanize = humanize.humanizer({
+    language: "short",
+    languages: {
+        short: {
+            y: () => "y",
+            mo: () => "mo",
+            w: () => "w",
+            d: () => "d",
+            h: () => "hr",
+            m: () => "min",
+            s: () => "sec",
+            ms: () => "ms",
+        },
+    },
+});
 
-    client.sToHMS = (time) => {
-        var h = Math.floor(time / 3600)
-        var m = Math.floor(time % 3600 / 60)
-        var s = Math.floor(time % 3600 % 60)
+module.exports = (client) => {
+    client.newError = (error) => {
+        const embed = new MessageEmbed().setDescription(error).setColor("#FF0000");
+        return embed;
+    };
 
-        var str = ""
-        h < 1 ? null : str += `${h}h`
-        m < 1 ? null : str += `${m}m`
-        s < 1 ? null : str += `${s}s`
-
-        return str
-    }
-}
+    client.humanize = (time) => {
+        return shortHumanize(time);
+    };
+};
